69723df7b555688027eda02c52247e93b6238f48
Patrick Taburet <taburet.patrick@gmail.com>
2024-02-23 16:21:43 +0100

    Create, read, update, delete.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -17,11 +17,11 @@ class MainController extends AbstractController
      */
     public function index(ArticlesRepository $repo): Response
     {
-        $datas = $repo-> findAll();
+        $data = $repo-> findAll();
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-            'datas'=> $datas
+            'data'=> $data
         ]);
     }
 
@@ -47,5 +47,51 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
             'form' => $form->createView()
         ]);
+    }
+
+    
+    /**
+    * @Route("/update/{id}", name="update", methods= {"GET", "POST"})
+    */
+    public function Update(Request $request, ArticlesRepository $repo, $id): Response
+    {
+        $article = $repo-> find($id);
+            
+        $form = $this->createForm(ArticleType::class, $article); // creation du form
+        $form -> handleRequest($request);  // Gestion des données envoyées
+        if ( $form->isSubmitted() && $form->isValid()){
+            $sendDatabase = $this->getDoctrine()
+                                 ->getManager();
+            $sendDatabase->persist($article);
+            $sendDatabase->flush();
+
+            $this->addFlash('notice', 'Modification réussie !!'); 
+
+            return $this->redirectToRoute('main');
+        }
+        return $this->render('main/updateForm.html.twig', [
+            'controller_name' => 'MainController',
+            'form' => $form->createView()
+        ]);
+    }
+    
+    
+    /**
+    * @Route("/delete/{id}", name="delete", methods= {"GET", "POST"})
+    */
+    public function Delete(ArticlesRepository $repo, $id): Response
+    {
+        $article = $repo-> find($id);
+            
+       
+            $sendDatabase = $this->getDoctrine()
+                                 ->getManager();
+            $sendDatabase->remove($article);
+            $sendDatabase->flush();
+
+            $this->addFlash('notice', 'Suppression réussie !!'); 
+
+    
+        return $this->redirectToRoute('main');
     }
 }
